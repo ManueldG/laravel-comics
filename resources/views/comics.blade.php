8cecc153f6b7671f2c9eea1e87468560c3df07a7
Manuel della Gala <manuel.dellagala@gmail.com>
2021-06-13 20:29:06 +0200

    ok

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,27 +4,32 @@
 <body>
         <div class="content">
             <div class="title m-b-md">
-
                 <h1>Comics</h1>
+            </div>
 
-                <div class="d-flex flex-wrap">
+                <div class="mx-3">
 
-                    @foreach ($comics as $comic)
-                    <div class="card">
-                        <img src="{{ $comic['image'] }}" class="card-img-top" alt="">
-                        <a href="{{ $comic['image-hero'] }}">Copertina </a>
-                        <a href="{{ $comic['image-cover']  }}">Cover</a>
-                            <div class="card-body">
-                                <h2 class="card-title">{{ $comic['title'] }}</h2>
-                            <h4>{{ $comic['price'] }}$</h4>
-                            <p class="card-text">{{trim(strip_tags($comic['body']),'&nbsp;')}}</p>
+                    <div class="d-flex flex-wrap ">
+                        @foreach ($comics as $comic)
+
+                            <div class="card">
+                                <img src="{{ $comic['image'] }}" class="card-img-top" alt="">
+                                <a href="{{ $comic['image-hero'] }}">Copertina </a>
+                                <a href="{{ $comic['image-cover']  }}">Cover</a>
+                                    <div class="card-body">
+                                        <h2 class="card-title">{{ $comic['title'] }}</h2>
+                                    <h4>{{ $comic['price'] }}$</h4>
+                                    <p class="card-text">{{trim(strip_tags($comic['body']),'&nbsp;')}}</p>
+                                    </div>
                             </div>
-                    </div>
-                    @endforeach
-                </div>
-        </div>
 
-    </div>
+                        @endforeach
+                    </div>
+
+                </div>
+
+
+        </div>
 </body>
 
 
